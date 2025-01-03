ebd96469555a3bd36a4ffc809eb01acff47dbb7b
AnjaliChathurani <108042163+AnjaliChathurani@users.noreply.github.com>
2025-01-03 11:56:18 +0530

    Create arr2.py
    by using array module

---
+++ arr2.py
@@ -0,0 +1,13 @@
+#add 5 integer numbers using an array
+
+numbers = []
+
+
+numbers.append(10)
+numbers.append(20)
+numbers.append(30)
+numbers.append(40)
+numbers.append(50)
+
+
+print("5 numbers are :",numbers)
